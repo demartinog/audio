5d62eb18e80899bcdea3c6730fbf1d2cd163f079
Tug Witt <gdwitt12@protonmail.ch>
2019-08-17 14:49:18 -0700

    updated

--- app.js
+++ app.js
@@ -15,9 +15,11 @@ app.get('/test', (req, res) => {
 
 app.get('/track', (req, res, err) => {
 	// generate file path/Users/tug/302/music/music-frequency-d3/audio/Odesza - Above The Middle.mp3
-	const filePath = '/Users/tug/302/music/music-frequency-d3/audio/Odesza - Above The Middle.mp3';
+	const filePath = __dirname + '/raw/Odesza - Above The Middle.mp3';
 	// get file size info
 	const stat = fs.statSync(filePath);
+
+	console.log(stat)
   
 	// set response header info
 	res.writeHead(200, {
@@ -28,48 +30,5 @@ app.get('/track', (req, res, err) => {
 	//create read stream
 	const readStream = fs.createReadStream(filePath);
 	// attach this stream with response stream
-	readStream.on('open', function () {
-		// This just pipes the read stream to the response object (which goes to the client)
-		console.log(res)
-    readStream.pipe(res);
+	readStream.pipe(res);
   });
-  });
-
-//register api calls
-app.use('/api/v1/', api);
-
-// send react app on / GET
-app.use(express.static(path.resolve(__dirname, './public/build/')));
-app.use(express.static(path.resolve(__dirname, './public/assets/')));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public/build/', './index.html'));
-});
-
-const server = http.createServer(app);
-const io = require('socket.io').listen(server, {
-  log: false,
-  agent: false,
-  origins: '*:*',
-  transports: ['websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling', 'polling']
-});
-
-io.on('connection', client => {
-
-  const stream = ss.createStream();
-
-  client.on('track', () => {
-    const filePath = path.resolve(__dirname, './private', './track.wav');
-    const stat = fs.statSync(filePath);
-    const readStream = fs.createReadStream(filePath);
-    // pipe stream with response stream
-		readStream.pipe(stream);
-		console.log('test')
-
-    ss(client).emit('track-stream', stream, { stat });
-  });
-  client.on('disconnect', () => {});
-});
-
-server.listen(process.env.PORT || '3001', function () {
-  console.log('Server app listening on port 3001!');
-});
